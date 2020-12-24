be8e75e66c4b030174cdacdf1c89929533ba0946
allan <allan.w.hart@gmail.com>
2020-12-24 10:40:20 -0700

    Simplified AuthorizationLayout

--- src/layout/AuthorizationLayout.js
+++ src/layout/AuthorizationLayout.js
@@ -1,26 +1,19 @@
 import PropTypes from 'prop-types';
 
-import React, { lazy, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { useTheme } from '@material-ui/core/styles';
 
 import SplitView from '@material-appkit/core/components/SplitView';
-import { isValue } from '@material-appkit/core/util/value';
 
 import ApplicationBar from 'components/ApplicationBar';
 
 import AppContext from 'AppContext';
 import paths from 'paths';
 
+import AuthorizationPage from 'pages/AuthorizationPage';
 
-const routes = [
-  {
-    exact: false,
-    path: paths.auth.index,
-    component: lazy(() => import('pages/AuthorizationPage')),
-  },
-];
 
 function AuthorizationLayout(props) {
   const context = useContext(AppContext);
@@ -38,26 +31,17 @@ function AuthorizationLayout(props) {
       placement="top"
     >
       <Switch>
-        {routes.map((routeInfo) => {
-          let exact = true;
-          if (isValue(routeInfo.exact)) {
-            exact = routeInfo.exact;
-          }
-          return (
-            <Route
-              exact={exact}
-              key={routeInfo.path}
-              path={routeInfo.path}
-              render={(routeProps) => (
-                <routeInfo.component
-                  location={props.location}
-                  match={routeProps.match}
-                  onMount={handleViewControllerMount}
-                />
-              )}
+        <Route
+          exact={false}
+          path={paths.auth.index}
+          render={(routeProps) => (
+            <AuthorizationPage
+              location={props.location}
+              match={routeProps.match}
+              onMount={handleViewControllerMount}
             />
-          );
-        })}
+          )}
+        />
         <Redirect to={paths.auth.login} />
       </Switch>
     </SplitView>
@@ -65,15 +49,9 @@ function AuthorizationLayout(props) {
 }
 
 AuthorizationLayout.propTypes = {
-  initialize: PropTypes.func,
   location: PropTypes.object.isRequired,
-  maxWidth: PropTypes.string,
   onMount: PropTypes.func.isRequired,
   onUnmount: PropTypes.func.isRequired,
-};
-
-AuthorizationLayout.defaultProps = {
-  maxWidth: 'md',
 };
 
 export default AuthorizationLayout;
