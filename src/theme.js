7e1463c1ca9f143b5c503b14731638a27501171a
Allan Hart <allan.w.hart@gmail.com>
2020-12-24 01:58:07 -0700

    Removed unused theme properties

--- src/theme.js
+++ src/theme.js
@@ -54,155 +54,6 @@ const theme = createMuiTheme({
     secondary: {
       main: '#B22234',
     },
-    text: {
-      disabled: 'rgba(0, 0, 0, 0.55)',
-    },
-  },
-
-  mixins: {
-    layout: {
-      split: {
-        display: 'grid',
-        gridAutoFlow: 'column',
-        gridTemplateColumns: '1fr 1fr',
-        gridGap: 16,
-      },
-
-      splitStack: {
-        display: 'grid',
-        gridAutoFlow: 'row',
-        gridTemplateColumns: '1fr',
-        gridGap: 16,
-
-        [baseTheme.breakpoints.up('md')]: {
-          gridAutoFlow: 'column',
-          gridTemplateColumns: '1fr 1fr',
-        },
-      },
-    },
-
-    status: {
-      successBgColor: {
-        backgroundColor: baseTheme.palette.success.main,
-      },
-      infoBgColor: {
-        backgroundColor: baseTheme.palette.info.main,
-      },
-      warningBgColor: {
-        backgroundColor: baseTheme.palette.warning.main,
-      },
-      errorBgColor: {
-        backgroundColor: baseTheme.palette.error.main,
-      },
-      cancelledBgColor: {
-        backgroundColor: baseTheme.palette.common.black,
-      },
-      successColor: {
-        color: baseTheme.palette.success.main,
-      },
-      infoColor: {
-        color: baseTheme.palette.info.main,
-      },
-      warningColor: {
-        color: baseTheme.palette.warning.main,
-      },
-      errorColor: {
-        color: baseTheme.palette.error.main,
-      },
-      cancelledColor: {
-        color: baseTheme.palette.common.black,
-      },
-    },
-
-    buttons: {
-      deleteButton: {
-        color: baseTheme.palette.error.main,
-      },
-
-      linkButton: {
-        minWidth: 'initial',
-        padding: 0,
-        '&:hover': {
-          backgroundColor: 'initial',
-        },
-      },
-    },
-
-
-    fullScreenDialog: {
-      dialogTitle: {
-        alignItems: 'center',
-        display: 'flex',
-        padding: baseTheme.spacing(1, 2),
-        position: 'relative',
-      },
-
-      heading: {
-        flex: 1,
-      },
-
-      dialogContent: {
-        padding: baseTheme.spacing(1, 2),
-      },
-
-      dialogActions: {
-        display: 'flex',
-        flexDirection: 'column',
-        padding: baseTheme.spacing(1),
-
-        [baseTheme.breakpoints.up('md')]: {
-          flexDirection: 'row',
-        },
-      },
-
-      commitButton: {
-        [baseTheme.breakpoints.up('md')]: {
-          order: 1,
-        },
-      },
-    },
-
-    toolbar: {
-      [baseTheme.breakpoints.down('md')]: {
-        minHeight: NAVBAR_HEIGHT,
-      },
-    },
-
-    pageTitle: {
-      fontSize: baseTheme.typography.pxToRem(24),
-      marginBottom: baseTheme.spacing(2),
-    },
-
-    metadataGrid: {
-      borderBottom: `1px solid ${baseTheme.palette.grey[400]}`,
-      borderTop: `1px solid ${baseTheme.palette.grey[400]}`,
-      paddingBottom: baseTheme.spacing(2),
-      paddingTop: baseTheme.spacing(2),
-      marginBottom: baseTheme.spacing(3),
-      marginTop: baseTheme.spacing(2),
-
-      '& > div:first-child': {
-        [baseTheme.breakpoints.down('sm')]: {
-          paddingBottom: baseTheme.spacing(2),
-        },
-
-        [baseTheme.breakpoints.up('md')]: {
-          paddingRight: baseTheme.spacing(1),
-        },
-      },
-
-      '& > div:not(:first-child)': {
-        [baseTheme.breakpoints.down('sm')]: {
-          borderTop: `1px solid ${baseTheme.palette.grey[400]}`,
-          paddingTop: baseTheme.spacing(2),
-        },
-
-        [baseTheme.breakpoints.up('md')]: {
-          borderLeft: `1px solid ${baseTheme.palette.grey[400]}`,
-          paddingLeft: baseTheme.spacing(2),
-        },
-      },
-    },
   },
 
   typography: {
@@ -248,52 +99,12 @@ const theme = createMuiTheme({
     width: SIDEBAR_WIDTH,
   },
 
-  scrollView: {
-    height: '100%',
-    overflow: 'auto',
-    width: '100%',
-  },
-
   sizes: {
     navigationController: {
       navbarHeight: NAVBAR_HEIGHT,
       toolbarHeight: TOOLBAR_HEIGHT,
     },
   },
-
-
-  // LIST DIALOG
-  listDialog: {
-    filterField: {
-      backgroundColor: '#FFF',
-      borderRadius: 4,
-      padding: '0 16px',
-      marginBottom: 16,
-    },
-
-    paper: {
-      minWidth: 320,
-      width: 480,
-    },
-
-    progressBar: {
-      height: 2,
-    },
-  },
-
-  // LIST VIEW
-  listView: {
-    list: {
-      padding: 0,
-      width: '100%',
-      height: '100%',
-      overflow: 'auto',
-    },
-    subheader: {
-      backgroundColor: '#e7e7e7',
-    },
-  },
-
 
   // PROPERTY LIST
   propertyList: {
@@ -346,39 +157,6 @@ const theme = createMuiTheme({
         padding: 0,
         width: 'initial',
       },
-    },
-  },
-
-  // PROPERTY TABLE
-  propertyTable: {
-    cell: {
-      fontSize: baseTheme.typography.pxToRem(13),
-      padding: `${baseTheme.spacing(0.5)}px ${baseTheme.spacing(1)}px`,
-    },
-
-    table: {
-      tableLayout: 'fixed',
-    },
-
-    labelCell: {
-      fontWeight: 500,
-      width: 150,
-
-      [baseTheme.breakpoints.up('md')]: {
-        width: 300,
-      },
-    },
-
-    rowInteractive: {
-      cursor: 'pointer',
-    },
-
-    rowOdd: {
-      backgroundColor: baseTheme.palette.grey[100],
-    },
-
-    selectionCell: {
-      width: 40,
     },
   },
 });
