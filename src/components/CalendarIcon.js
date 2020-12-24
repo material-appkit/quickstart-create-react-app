79eb21029e6ef73bca92256b589e19ce75fff507
Allan Hart <allan.w.hart@gmail.com>
2020-12-24 00:15:11 -0700

    Simplified layout using flexbox

--- src/components/CalendarIcon.js
+++ src/components/CalendarIcon.js
@@ -13,46 +13,40 @@ import { makeStyles } from '@material-ui/core/styles';
 const styles = makeStyles((theme) => ({
   icon: {
     fontSize: '1em',
-    display: 'block',
-    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     width: 100,
     height: 100,
     backgroundColor: '#fff',
     borderRadius: '0.6em',
     boxShadow: '0 1px 0 #bdbdbd, 0 2px 0 #fff, 0 3px 0 #bdbdbd, 0 4px 0 #fff, 0 5px 0 #bdbdbd, 0 0 0 1px #bdbdbd',
-    marginRight: theme.spacing(2),
     overflow: 'hidden',
-
-    '& *': {
-      display: 'block',
-      width: '100%',
-      fontSize: '1em',
-      fontWeight: 'bold',
-      textAlign: 'center',
-      position: 'absolute',
-    },
+    textAlign: 'center',
   },
 
   strong: {
-    top: 0,
     padding: '0.4em 0',
     color: '#fff',
     backgroundColor: theme.palette.primary.main,
     borderBottom: `1px dashed ${theme.palette.secondary.dark}`,
     boxShadow: `0 2px 0 ${theme.palette.secondary.dark}`,
+    width: '100%',
   },
 
   span: {
-    fontSize: '2.8em',
-    letterSpacing: '-0.05em',
-    paddingTop: '0.8em',
     color: '#2f2f2f',
+    fontSize: theme.typography.pxToRem(28),
+    fontWeight: 500,
+    letterSpacing: '-0.05em',
+    lineHeight: 1,
+    margin: '12px 0 2px',
   },
 
   em: {
-    bottom: '0.3em',
     color: theme.palette.secondary.main,
     fontStyle: 'normal',
+    fontWeight: 500,
   },
 }));
 
@@ -60,7 +54,7 @@ function CalendarIcon(props) {
   const classes = styles();
 
   const [date] = useState(() => dayjs(props.date));
-    
+
   return (
     <time dateTime={props.date} className={classes.icon}>
       <strong className={classes.strong}>{date.format('MMMM')}</strong>
