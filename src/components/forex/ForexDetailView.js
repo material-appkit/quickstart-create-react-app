2f5c28853369d59710114c24afe7f1bd815922e9
Allan Hart <allan.w.hart@gmail.com>
2020-12-26 17:33:13 -0700

    Use fixed item height

--- src/components/forex/ForexDetailView.js
+++ src/components/forex/ForexDetailView.js
@@ -102,6 +102,7 @@ function ForexDetailView(props) {
         loadingVariant="linear"
         listItemComponent={ForexHistoryListItem}
         windowed
+        windowedListItemHeight={110}
       />
     </SplitView>
   );
